b67c8cffa8ed8622d71db815d288989cb285b48a
mohammad mekawy <mohammad.mekawy92@gmail.com>
2020-10-27 21:36:38 +0200

    up icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,5 +19,16 @@ $(document).ready(function () {
     $(".vol-btn").on("click", function () {
         $(this).find(".fas").toggleClass("fa-plus")
     })
+    var myUpButton = $("div.up");
+    $(window).scrollTop() > 120 ? myUpButton.fadeIn() : myUpButton.fadeOut();
+    $(window).on("scroll", function () {
+        $(window).scrollTop() > 120 ? myUpButton.fadeIn() : myUpButton.fadeOut();
+    });
+
+    myUpButton.click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
 
 });
